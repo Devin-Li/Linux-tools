02546b62664e9e23d6a2f87d52b0968e8a9dc534
root <root@localhost.localdomain>
2019-03-19 16:31:31 +0800

    add install Aliyun yum resource script

---
+++ replace-default-yumrepo.sh
@@ -0,0 +1,14 @@
+#This script reference to Aliyun offical document: https://opsx.alibaba.com/mirror->帮助
+
+#CentOS Linux release 7.5.1804 (Core)
+mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.back
+echo -e "\033[31mCannot find original repo resource or it has been renamed \033[0m"
+release=`cat /etc/centos-release|cut -d " " -f 4|cut -c 1`
+curl -o /etc/yum.repos.d/CentOS-Base.repo -L http://mirrors.aliyun.com/repo/Centos-${release}.repo
+
+#cache is in /var/cache/yum, need to be clean after updating yum resource.
+echo -e "\033[31m Clean the yum cache...\033[0m"
+sudo yum clean all
+echo -e "\033[31mRebuild yum cache\033[0m"
+sudo yum makecache
+
